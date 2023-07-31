6bf86e5b4f869b9bbf430f2e879fb0fa0d3f296d
Jeonghun Cho <jcho@knu.ac.kr>
2023-07-31 19:25:23 +0900

    update

--- src/function/algebra/simplifyConstant.js
+++ src/function/algebra/simplifyConstant.js
@@ -386,11 +386,12 @@ export const createSimplifyConstant = /* #__PURE__ */ factory(name, dependencies
             if (node.isTrigonometric) {
               const args0 = node.args[0]
               if (args0.isOperatorNode) {
-                if (args0.isBinary && (args0.fn === 'add') || (args0.fn === 'subtract')) {
-                  // Polynomial.
-                  // Nothing to do.
-                } else if (args0.isBinary) {
-                  if (args0.fn === 'multiply') {  // sin((1/2)*x)
+                //console.log("TEST010", args0)
+                if (args0.isBinary()) {
+                  //console.log("TEST100", args0)
+                  if (args0.fn === 'add' || args0.fn === 'subtract') {
+                    // Polynomial
+                  } else if (args0.fn === 'multiply') {  // sin((1/2)*x)
                     // console.log("TEST100", args0)
                     const args00 = args0.args[0]
                     const args01 = args0.args[1]
@@ -398,12 +399,28 @@ export const createSimplifyConstant = /* #__PURE__ */ factory(name, dependencies
                       const new_args00 = new OperatorNode('*', 'multiply', [args00.args[0], args01])
                       const new_args0 = new OperatorNode('/', 'divide', [new_args00, args00.args[1]])
                       return new FunctionNode(node.name, [new_args0])
+                    } else if (args00.isOperatorNode && args00.isUnary() && args00.fn === 'unaryMinus') {
+                      console.log("TEST110", args00)
                     }
                   } else if (args0.fn === 'divide') {
 
                   }
-                } else if (args0.isUnary && args0.fn === 'unaryMinus') {
-
+                } else if (args0.isUnary() && args0.fn === 'unaryMinus') { // sin(-(1/2)*x)
+                  //console.log("TEST333", args0)
+                  const args1 = args0.args[0]
+                  if (args1.fn === 'multiply') {  
+                    // console.log("TEST200", args1)
+                    const args00 = args1.args[0]
+                    const args01 = args1.args[1]
+                    if ((args00.isOperatorNode && args00.fn === 'divide') && args01.isSymbolNode) {
+                      const new_args00 = new OperatorNode('*', 'multiply', [args00.args[0], args01])
+                      const new_args1 = new OperatorNode('/', 'divide', [new_args00, args00.args[1]])
+                      const new_arg = new OperatorNode('-', 'unaryMinus', [new_args1])
+                      return new FunctionNode(node.name, [new_arg])
+                    } else if (args00.isOperatorNode && args00.isUnary() && args00.fn === 'unaryMinus') {
+                      //console.log("TEST210", args00)
+                    }
+                  }                  
                 } else {
                   // console.log("Error - Display an expression in trigonometric function\n", args0)
                 }
